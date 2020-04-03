b73d652828560b508a00507b6d066c394791853e
Forenche <prahul2003@gmail.com>
2020-04-03 14:09:24 +0530

    initial commit

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -81,7 +81,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Forenche/One4uBot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "master")
+    "UPSTREAM_REPO_BRANCH", "sql-extended")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
